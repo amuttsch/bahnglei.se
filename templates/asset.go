20b511d7c1185fe49d0f07d3dbf4fc16fe21ed76
Andreas Muttscheller <andreas.muttscheller@codecentric.de>
2024-10-24 17:57:45 +0200

    Add caching for assets, preload css, images and font

--- templates/asset.go
+++ templates/asset.go
@@ -1,14 +1,11 @@
 package templates
 
 import (
-	"fmt"
-
 	"github.com/benbjohnson/hashfs"
 )
 
 var AssetFS *hashfs.FS
 
 func Asset(name string) string {
-	fmt.Printf("Asset: %s", name)
-	return "/assets/" + AssetFS.HashName(name)
+	return AssetFS.HashName("assets/" + name)
 }

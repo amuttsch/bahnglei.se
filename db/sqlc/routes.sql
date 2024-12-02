0e463cc5d99adc40161a9af36d23f4eafaeb59aa
Andreas Muttscheller <andreas.muttscheller@codecentric.de>
2024-12-02 22:58:12 +0100

    Update iso code in routes

--- db/sqlc/routes.sql
+++ db/sqlc/routes.sql
@@ -1,7 +1,7 @@
 -- name: CreateRoute :one
 insert into routes (route_id, stop_position_id, from_station, to_station, via, ref, name, operator, network, service, country_iso_code) 
   values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
-  on conflict (route_id, stop_position_id) do update set from_station = $3, to_station = $4, via = $5, ref = $6, name = $7, operator = $8, network = $9, service = $10, updated_at = CURRENT_TIMESTAMP
+  on conflict (route_id, stop_position_id) do update set from_station = $3, to_station = $4, via = $5, ref = $6, name = $7, operator = $8, network = $9, service = $10, updated_at = CURRENT_TIMESTAMP, country_iso_code = $11
   returning *;
  
 -- name: FindRoutesForStopPosition :many
